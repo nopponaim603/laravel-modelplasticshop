8e6a42d1b003f3648e97ae62437c120a727001a8
AIMMY-HOMEPC\noppo <noppon_aim@hotmail.com>
2021-09-06 22:21:30 +0700

    Add All Model Add Customer, Employees Detail and Relation

---
+++ app/Models/Employees.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employees extends Model
+{
+    use HasFactory;
+    protected $table = "employees";
+    
+    public function SalesRep()
+    {
+        return $this->hasMany('App\Models\Customer');
+    }
+}
